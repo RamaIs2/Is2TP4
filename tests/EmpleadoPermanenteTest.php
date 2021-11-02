14fa9fa654eaa5eb05350b9aebe4cbeeae06e431
RamaIs2 <rama.garciae86@gmail.com>
2021-11-02 17:43:00 -0300

    Finalizando

--- tests/EmpleadoPermanenteTest.php
+++ tests/EmpleadoPermanenteTest.php
@@ -7,37 +7,52 @@ public function crear($nombre = "Fulano", $apellido = "Detal", $dni = 123, $sala
         $empp = new \App\EmpleadoPermanente($nombre,$apellido,$dni,$salario,$fechaingreso);
 	    return $empp;
 	}
- public function testobtenerlafechadeingreso()
-{	
-      $m = $this->crear("Fulano","Detal",123,2000,null);
-      $this->assertEquals((new DateTime())->format('Y-m-d\TH:i'), ($m->getFechaIngreso())->format('Y-m-d\TH:i'));
-}
-   public function testcalcularlacomision()
-   {
-		$m = $this->crear("Fulano","Detal",123,2000,null);
-	    $this->assertEquals("0%",$m-> calcularComision());
-	}
-	public function testcalculoingresototal()
-	{
-	 $m = $this->crear("Fulano","Detal",123,2000,null);
-	    $this->assertEquals(2000,$m->calcularIngresoTotal());
-	}
-	 public function testempleadovariosañosdeantiguedad()
-	 {
-		 $t= $this->crear("Fulano","Detal",123,2000,null);
-		 $this->assertEquals(0,$t->calcularAntiguedad());
-	 }
-	 public function testempleadosinpropocionarlafechadeingreso()
-	 {
-	 $t= $this->crear("Fulano","Detal",123,2000,null);
-	 $this->assertEquals(0,$t->calcularAntiguedad());
+ public function testRetornarlaFechadeIngreso() {
+        $fechaIngreso = new DateTime('2017-07-14');
+        $c = $this->crear("Fulano", "Detal", 123, 2000, $fechaIngreso);
+        $this->assertEquals('2017-07-14', $c->getfechaIngreso()->format("Y-m-d"));
+ }
+     public function testCalculaComision() {
+        $fechaIngreso = new DateTime('2017-07-14');
+        $c = $this->crear("Fulano", "Detal", 123, 2000, $fechaIngreso);
+        $fechaActual = new DateTime();
+        $antiguedad = $fechaActual->diff($fechaIngreso);
+        $this->assertEquals($antiguedad->y, $c->calcularAntiguedad());
+        $this->assertEquals("{$antiguedad->y}%", $c->calcularComision());
+
      }
-  public function testfechaposterioraladehoy()
-  {
- 
-  $this->expectException(\Exception::class);
-  $m = $this->crear(null);
-  $this-> $m > getfechaIngreso();
-}
-}
+	 		
+    public function testCalcularlanAntiguedad() {
+         $fechaIngreso = new DateTime('2017-07-14');
+         $c = $this->crear("Fulano", "Detal", 123, 2000, $fechaIngreso);
+         $fechaActual = new DateTime();
+         $antiguedad = $fechaActual->diff($fechaIngreso);
+        $this->assertEquals($antiguedad->y, $c->calcularAntiguedad());
+     }
+       public function testAntiguedadConDiacero()
+     {
+       $fechaIngreso = new DateTime();
+       $c = $this->crear("Fulano", "Detal", 123, 2000, $fechaIngreso);
+       $fechaActual = new DateTime();
+       $antiguedad = $fechaActual->diff($fechaIngreso);
+       $this->assertEquals($antiguedad->y, $c->calcularAntiguedad());
+       $this->assertEquals("0%", $c->calcularComision());
+     }
+
+       public function testAntiguedadFechaFutura()
+     {
+        $this->expectException(\Exception::class);
+        $fechaIngreso = new DateTime('2023-05-05');
+        $fe = $this->crear("Fulano", "Detal", 123, 2000, $fechaIngreso);
+        $fechaActual = new DateTime();
+        $antiguedad = $fechaActual->diff($fechaIngreso);
+     }
+
+       public function testCalcularIngresoTotal() 
+     {
+        $antiguedad = 20;
+        $ing = $this->crear("Fulano", "Detal", 123, 2000 + $antiguedad);
+        $this->assertEquals(2020, $ing->calcularIngresoTotal());
+     }
+ }
 
